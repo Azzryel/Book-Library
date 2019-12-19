78b1917887a14d2d5bb1669a39c81d2ec2fdb8ab
Azzryel <AzryelShroud@gmail.com>
2019-12-19 21:56:17 +0100

    add local storage and readme.md

--- main.js
+++ main.js
@@ -6,6 +6,47 @@ const form = document.querySelector("#book-form");
 
 let myLibrary = [];
 
+if (storageAvailable('localStorage') && (JSON.parse(localStorage.getItem('myLibrary')) == null)) {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+
+}
+else if (storageAvailable('localStorage')) {
+  myLibrary = JSON.parse(localStorage.getItem('myLibrary'))
+
+//used a place holder for element positions, so since they get out of synch 
+//deleted elements in array and removed the null ones out of them
+
+  myLibrary = myLibrary.filter(function (el) {
+    return el != null;
+  });
+
+  let n = 0;
+  myLibrary.forEach ((e) => {
+    myLibrary[n].place = n;
+    createTemplate(e, n)
+    n++;
+  });
+}
+
+
+if (myLibrary == "") {
+  //test books
+const book1 = new Books('Eisenhorn', 'Dan Abnett', "768", "did")
+const book2 = new Books('Ravenor', 'Dan Abnett', "900", "did")
+const book3 = new Books('The Ultramaries', 'Graham McNeill', "989", "didNot")
+const book4 = new Books('Pariah', 'Dan Abnett', "408", "didNot")
+book1.img = "https://images-na.ssl-images-amazon.com/images/I/51iQf9TinNL._SX324_BO1,204,203,200_.jpg";
+book2.img ="https://images-na.ssl-images-amazon.com/images/I/71tm70Ez1HL.jpg";
+book3.img = "https://dave430.files.wordpress.com/2015/10/ultramarined-omnibus.jpg";
+book4.img = "https://sonsofcorax.files.wordpress.com/2013/09/eisenhorn-ravenor-01-pariah.jpg";
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
+}
+
+
+
 function Books(title, author, pages, read, img, place) {
   this.title = title
   this.author = author
@@ -16,46 +57,29 @@ function Books(title, author, pages, read, img, place) {
   this.info = function () {
     info = `${title} by ${author}, ${pages} pages`;
       return info;
-    }
-  this.status = function() {
-    this.read ? true : false;
   }
 }
 
-
 function addBookToLibrary(book) {
   myLibrary.push(book);
+
+  if (storageAvailable('localStorage')) {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+  }
+
   renderLast(book);
 }
 
-//test books
-const book1 = new Books('Eisenhorn', 'Dan Abnett', "768", "did")
-const book2 = new Books('Ravenor', 'Dan Abnett', "900", "did")
-const book3 = new Books('The Ultramaries', 'Graham McNeill', "989", "didNot")
-const book4 = new Books('book', 'someone', "99", "did")
-book1.img = "https://images-na.ssl-images-amazon.com/images/I/51iQf9TinNL._SX324_BO1,204,203,200_.jpg";
-book2.img ="https://images-na.ssl-images-amazon.com/images/I/71tm70Ez1HL.jpg";
-book3.img = "https://dave430.files.wordpress.com/2015/10/ultramarined-omnibus.jpg";
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-
-
-//not used now
-let render = ((array) => { 
-    array.forEach ((e) => {
-        createTemplate(e)
-    });
-}); 
 
 function renderLast(e) {
-  createTemplate(e)
+  n = myLibrary.length-1;
+  createTemplate(e, n)
 }
 
-function createTemplate (e) {
+function createTemplate (e, n) {
     const bookTemplate = document.createElement("div");
     bookTemplate.className = "books";
-    bookTemplate.setAttribute("data-place", `${myLibrary.length-1}`);
+    bookTemplate.setAttribute("data-place", n);
     displayBooks.appendChild(bookTemplate);
     const bookInfo = document.createElement("div");
     bookInfo.className = "book-info";
@@ -67,22 +91,22 @@ function createTemplate (e) {
     const deleteBtn = document.createElement("button");
     deleteBtn.id = "delete";
     deleteBtn.textContent = "delete";
-    deleteBtn.setAttribute("data-place", `${myLibrary.length-1}`);
+    deleteBtn.setAttribute("data-place", n);
     bookTemplate.appendChild(deleteBtn);
     const readBtn = document.createElement("button");
-    readBtn.setAttribute("data-place", `${myLibrary.length-1}`);
+    readBtn.setAttribute("data-place", n);
     readBtn.id = "read";
     readBtn.textContent = "read";
     bookTemplate.appendChild(readBtn);
     const readStatus = document.createElement("img");
     readStatus.id = "read-status";
     readStatus.setAttribute("src", "./img/checkbox.png");
-    if (myLibrary[`${myLibrary.length-1}`].read == "didNot") readStatus.style.display="none";
+    if (myLibrary[n].read == "didNot") readStatus.style.display="none";
     bookTemplate.appendChild(readStatus);
     const para = document.createElement("p");
     bookInfo.appendChild(para);
 
-    para.textContent = `${e.info()}`
+    para.textContent = `${e.title} by ${e.author}, ${e.pages} pages`;
 
     deleteBtn.addEventListener("click",(e) => {
       let place = e.target.getAttribute('data-place');
@@ -109,10 +133,16 @@ function createTemplate (e) {
       }
       else if (read == "didNot") {
         myLibrary[locate].read="did";
-        read="did";
         e.target.nextSibling.style.display = "block";
+      }     
+      
+      if (storageAvailable('localStorage')) {
+        localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
       }
+      
     });
+
+
   }
 
 
@@ -128,6 +158,7 @@ addBtn.addEventListener("click", (e) => {
   let read = document.getElementById("read-check").value;
   let img = document.getElementById("img").value;
   let place = `${myLibrary.length}`
+  
 
 
   if ((title == "") || (author == "") || ( isNaN(pages)) || (pages < 1) ){
@@ -145,16 +176,17 @@ addBtn.addEventListener("click", (e) => {
   resetFormValue();
 });
 
-function validateForm (title, author, pages, img) {
-
-}
-
 cancelBtn.addEventListener("click", () => {
   resetFormValue();
 });
 
 function remove(place) { 
-    myLibrary.splice(place, 1);
+    delete myLibrary[place];
+
+    if (storageAvailable('localStorage')) {
+      localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+    }
+  
     let div = document.querySelector(`div[data-place="${place}"]`)
     displayBooks.removeChild(div);
   }
@@ -169,29 +201,27 @@ function resetFormValue() {
   document.getElementById("img").value = "";
 }
 
-
-/*
-form.addEventListener("submit", () => {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").value;
-
-  const book = new Books(title, author, pages, read);
-  addBookToLibrary(book);
-  console.log(myLibrary)
-  alert(`submit ${book}`)
-});
-
-function submitForm () {
-  
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
-  let read = document.getElementById("read").value;
-
-  let book = new Books(title, author, pages, read);
-  addBookToLibrary(book)
-  alert("submitted")
+function storageAvailable(type) {
+  var storage;
+  try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+  }
+  catch(e) {
+      return e instanceof DOMException && (
+          // everything except Firefox
+          e.code === 22 ||
+          // Firefox
+          e.code === 1014 ||
+          // test name field too, because code might not be present
+          // everything except Firefox
+          e.name === 'QuotaExceededError' ||
+          // Firefox
+          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          // acknowledge QuotaExceededError only if there's something already stored
+          (storage && storage.length !== 0);
+  }
 }
-*/
